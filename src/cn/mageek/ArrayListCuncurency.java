91ec71208c1ec9987ffb7e6bc276de8971d697fd
queenk <qiukang@mail.ustc.edu.cn>
2017-11-12 21:26:15 +0800

    Initial commit

---
+++ src/cn/mageek/ArrayListCuncurency.java
@@ -0,0 +1,32 @@
+package cn.mageek;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public class ArrayListCuncurency {
+    // 偶尔能够正常运行 得2000
+    static List<Integer> arrayList = new ArrayList<>(10);
+
+    // 一直能够正常运行
+//    static List<Integer> arrayList = new Vector<>(10);
+
+    public static class AddThread implements Runnable{
+
+        @Override
+        public void run() {
+            for (int i=0;i<1000;i++){
+                arrayList.add(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(new AddThread());
+        Thread t2 = new Thread(new AddThread());
+        t1.start();t2.start();
+        t1.join();t2.join();
+        System.out.println(arrayList.size());
+    }
+
+}
